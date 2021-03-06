13e3743a1543de5cd625ff04e05813efc4adef00
Stephen Tobin <stobin1465@gmail.com>
2021-03-06 14:59:42 -0500

    Stephens illegal project

---
+++ src/Illegal.java
@@ -0,0 +1,12 @@
+
+public class Illegal
+{      
+    public static void main (String args[]) {
+        float floaty = 4;
+        String blue = "Blue";
+        
+        System.out.println(floaty); 
+        System.out.println(blue);
+    }
+
+}
